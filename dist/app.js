1632703e8dc7bec52779c75304331c63bb0efd3d
Kelly Stoops <kstoops815@gmail.com>
2017-10-14 11:10:01 -0500

    base card styling

--- dist/app.js
+++ dist/app.js
@@ -163,8 +163,25 @@ let outputDiv = $("#dinosaur");
 
 const createDomString = (dinosaurs) => {
 	let dinoString = "";
-		dinoString += `<div class="shitGoesHere">`;
+		dinoString += `<div class="${dinosaurs.info === 'Carnivore' ? 'card-bad' : 'card-good'}">`;
 		dinoString +=	`<h1>${dinosaurs.type}</h1>`;
+		dinoString +=	`<h4>${dinosaurs.bio}</h4>`;
+		if (dinosaurs.info === 'Carnivore') {
+			dinoString += `<h4>Has some tasty snacks.</h4>`;
+		} else {
+			dinoString += `<h4>Has some adorable (debatable) friends.</h4>`;
+		}
+		dinoString += `<div class="card-holder">`;
+		dinosaurs.snacks.forEach((cat) => {
+			dinoString += `<div class="card">`;
+			dinoString += `<h5>${cat.name}</h5>`;
+			dinoString += `<div class="card-img">`;
+			dinoString += `<img src=${cat.imageUrl}>`;
+			dinoString += `</div>`;
+			dinoString += `<p class="card-description">${cat.specialSkill}</p>`;
+			dinoString += `</div>`;
+		});
+		dinoString += `</div>`;
 		dinoString += `</div>`;
 	printToDom(dinoString);
 }; 	
